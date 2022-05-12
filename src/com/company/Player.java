ff4c45d55b277b576cee3bf6904b1998fcecbcd5
Iustinian Serban <iustinianserban@gmail.com>
2022-05-12 22:37:49 +0300

    Implement jump

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -7,10 +12,12 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Player {
     private BufferedImage image;
-    private Rectangle position;
+    private javafx.scene.shape.Rectangle rectangle;
     private int score;
     private int step = 50;
     private int width = 100;
@@ -18,7 +25,7 @@ public class Player {
 
     public Player(){
         loadImage();
-        position = new Rectangle(0, 11, width, height);
+        rectangle = new javafx.scene.shape.Rectangle(0, 11, width, height);
         score = 0;
     }
 
@@ -32,43 +39,51 @@ public class Player {
     public void draw(Graphics g, ImageObserver observer) {
         g.drawImage(
                 image,
-                position.x * step,
-                position.y * step,
+                rectangle.xProperty().intValue() * step,
+                rectangle.yProperty().intValue() * step,
                 observer
         );
     }
 
-    public void keyPressed(KeyEvent e){
+    public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
 
         System.out.print(key + ": ");
 
         if(key == KeyEvent.VK_UP){
-            position.translate(0, -1);
+            rectangle.setY(rectangle.getY() - 3);
+            TimerTask task = new TimerTask() {
+                public void run() {
+                    rectangle.setY(rectangle.getY() + 3);
+                }
+            };
+            Timer timer = new Timer("Timer");
+
+            long delay = 500L;
+            timer.schedule(task, delay);
             System.out.println("UP");
         }
         if(key == KeyEvent.VK_RIGHT){
-            position.translate(1, 0);
+            rectangle.setX(rectangle.getX() + 1);
             System.out.println("RIGHT");
         }
         if(key == KeyEvent.VK_DOWN){
-            position.translate(0, 1);
+            rectangle.setY(rectangle.getY() + 1);
             System.out.println("DOWN");
         }
         if(key == KeyEvent.VK_LEFT){
-            position.translate(-1, 0);
+            rectangle.setX(rectangle.getX() - 1);
             System.out.println("LEFT");
         }
-
-        System.out.println(getPosition().toString());
+        System.out.println(getRectangle().toString());
     }
 
     public void tick(){
-        if(position.x < 0) position.x = 0;
-        else if(position.x + 2 >= Board.COLS ) position.x = Board.COLS - 2;
+        if(rectangle.getX() < 0) rectangle.setX(0);
+        else if(rectangle.getX() + 2 >= Board.COLS ) rectangle.setX(Board.COLS - 2);
 
-        if(position.y < 0) position.y = 0;
-        else if(position.y + 2 >= Board.ROWS - Ground.height + 1) position.y = Board.ROWS -  Ground.height  - 2;
+        if(rectangle.getY() < 0) rectangle.setY(0);
+        else if(rectangle.getY() + 2 >= Board.ROWS - Ground.height + 1) rectangle.setY(Board.ROWS -  Ground.height  - 2);
     }
 
     public String getScore(){
@@ -79,7 +94,7 @@ public class Player {
         score += amount;
     }
 
-    public Rectangle getPosition(){
-        return position;
+    public Rectangle getRectangle(){
+        return rectangle;
     }
 }
